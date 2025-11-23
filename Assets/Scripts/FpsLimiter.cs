44905e91179ff84f9a921ac057c54fe6446485bc
Eldar <venando@ex.ua>
2025-11-23 16:43:08 +0200

    Fixed building issues

--- Assets/Scripts/FpsLimiter.cs
+++ Assets/Scripts/FpsLimiter.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class FpsLimiter : MonoBehaviour
 {
     [SerializeField] private bool active = false;
+    [SerializeField] private int limit = 240;
+    [SerializeField] private bool vSync = true;
 
     private void Start()
     { 
         if (active)
         {
-            Application.targetFrameRate = 30;       
+            Application.targetFrameRate = limit;
+            QualitySettings.vSyncCount = vSync ? 1 : 0;      
+            Time.fixedDeltaTime = 0.02f;
+            Time.maximumDeltaTime = 0.1f; 
         }
     }
 }
--- Assets/Scripts/Ui/GameUiManager.cs
+++ Assets/Scripts/Ui/GameUiManager.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 
 
